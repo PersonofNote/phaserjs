e211fede8759501bf53cb12dc2d8566d88d93d7a
Jessica L.T. Martin <apersonofnote@gmail.com>
2020-09-15 20:53:15 -0700

    Update playscreen

--- src/playScreen.js
+++ src/playScreen.js
@@ -12,10 +12,12 @@ export default class playScreen extends Phaser.Scene {
         this.add.image(400, 400, "bg_sky").setDisplaySize(800, 800);
         const npc = new NPC(this, 400, 400, "npc_walkDown", 0, "happy");
         console.log(npc);
-        const plant = new Plant(this, 200, 200, "grass1", 0, 3, ["fiber", "fiber", "seed"]);
-        const plant1 = new Plant(this, 300, 200, "grass1", 0, 3, ["fiber"]);
-        const plant2 = new Plant(this, 600,250, "tree_orange", 0, 5, ["wood", "wood", "fiber"] );
         const fiber = new Craftable(this, 100,500, "fiber", 0, "Fiber", "From a plant");
+        const plant = new Plant(this, 200, 200, "grass1", 0, 3, [fiber, fiber]);
+        //const plant1 = new Plant(this, 300, 200, "grass1", 0, 3, ["fiber"]);
+        const tree = new Plant(this, 600,250, "tree_orange", 0, 5, ["wood", "wood", "fiber"] );
+        tree.animations.add('wobble', [0,1,0,2,0,1,0,2,0], 24);
+ 
     }
 
     update() {
